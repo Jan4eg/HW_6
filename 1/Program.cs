faff6571e390ea9c1aad2407ddb70183dccd0f46
Iana Glebova <letdrown@gmail.com>
2023-04-23 20:23:22 +0300

    Добавлены 2 папки с задачами

---
+++ 1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+int [] Read()
+{
+string numstring;
+System.Console.WriteLine("Введите числа через запятую");
+numstring = Console.ReadLine();
+string [] arraystring = numstring.Split(", ");
+int [] array = new int [arraystring.Length];
+for (int i = 0; i < arraystring.Length; i++)
+{
+    array[i] = int.Parse(arraystring[i]);
+}
+return array;
+System.Console.WriteLine(string.Join("; ", array));
+}
+
+/*void Print(int [] array)
+{
+    System.Console.WriteLine(string.Join("; ", array));
+}
+
+Print(Read());*/
+
+int PositiveNum(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+int count = PositiveNum(Read());
+System.Console.WriteLine(count);
+
+
---
+++ 2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+
+double Read (string arg)
+{
+System.Console.Write($"Введите {arg}: ");
+double a = double.Parse(Console.ReadLine());
+return a;
+}
+
+double b1 = Read("b1");
+double k1 = Read("k1");
+double b2 = Read("b2");
+double k2 = Read("k2");
+
+void IntersectionPoints(double b1, double b2, double k1, double k2)
+{
+    double x = (b2-b1)/(k1-k2);
+    System.Console.WriteLine("x = " + x);
+    double y1 = k1*x+b1;
+    System.Console.Write("Точка пересечения: (" + x + "; " + y1 + ")");
+}
+
+IntersectionPoints(b1, b2, k1, k2);
